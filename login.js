a7f80769d8f4dedec07f64b985b07ccfaadb1460
Hoang Trung Thang <Hoangtrungthangtrau4@gmail.com>
2025-05-03 03:43:40 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,6 @@
+function change_model() {
+    const halfForm = document.querySelector('.half_form');
+    const particleOne = document.querySelector('.particle');
+    halfForm.classList.toggle('active');
+    particleOne.classList.toggle('active');
+}
